f0822cd4cc3245d30e2c4a2f8b8304b56cb56042
Nathan Eloe <powerofazure@gmail.com>
2020-09-03 10:31:41 -0500

    Add day 4 of pointers example

---
+++ fall20/pointers4.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+	int x = 5;
+	printf("Size of int: %d\n", sizeof(int));
+	printf("x is: %d\n", x);
+
+	int * ptr;
+	printf("Size of ptr: %d\n", sizeof(int *));
+	ptr = &x;
+	printf("ptr is: %x\n", ptr);
+	printf("Dereferencing ptr: %d\n", *ptr);
+	*ptr = 15;
+	printf("x is: %d\n", x);
+	
+	int arr[5] = {};
+
+	int * dynarr = malloc(10 * sizeof(int));
+	printf("dynarr: %x\n", dynarr);
+	dynarr[0] = 10;
+	dynarr[1] = 12;
+	printf("%d %d\n", dynarr[0], dynarr[1]);
+	printf("*dynarr is: %d\n", *dynarr);
+	free(dynarr);
+
+	char ** strlist = malloc( 5 * sizeof(char*));
+	for(int i=0; i<5; i++)
+		strlist[i] = malloc(10 * sizeof(char));
+	strcpy(strlist[0], "hello");
+	printf("%s\n", strlist[0]);
+	printf("%c\n", **strlist);
+	printf("%s\n", *strlist);
+
+	for (int i=0; i<5; i++)
+		free(strlist[i]);
+	free(strlist);
+
+	printf("Enter a size: ");
+	int num;
+	scanf("%d", &num);
+
+
+	int * userarr = malloc(num * sizeof(int));
+	free(userarr);
+
+
+
+	return 0;
+}
+
