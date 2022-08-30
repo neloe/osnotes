9f545b2c8312ebc25440602d05d7855d3586c7c5
Nathan Eloe <nathane@nwmissouri.edu>
2022-08-30 09:38:41 -0500

    Demo hello world

---
+++ fall22/helloworld.c
@@ -0,0 +1,16 @@
+// This is a comment
+/* This is a multi
+ * line comment
+ */
+#include <stdio.h>
+
+int main(/*int argc, char* argv[]*/)
+{
+	printf("Hello World");
+	/*(while (1 == 1)
+		printf("hi again!\n");
+	else
+		printf("waitwat?\n");
+	*/
+	return 0;
+}
