4b193fc0bed275b2f040d4c70b13e94a24d90d3f
Nathan Eloe <nathane@nwmissouri.edu>
2022-09-01 10:51:02 -0500

    Pointer notes day 1

---
+++ fall22/pointers/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+void swap(int * a, int * b);
+
+int main()
+{
+    int num;
+    int *addr;
+    num = 5;
+    printf("%lu\n", sizeof(num));
+    printf("%lu\n", sizeof(addr));
+    addr = &num;
+    printf("Addr of num: %p, addr: %p\n", &num, addr);
+    printf("Address of addr: %p\n", &addr);
+    printf("Value at address %p: %d\n", addr, *addr);
+    *addr = 10;
+    printf("Values; num = %d, *addr = %d\n", num, *addr);
+    int num2 = 42;
+    printf("%d, %d\n", num, num2);
+    swap(&num, &num2);
+    printf("%d, %d\n", num, num2);
+    // C style arrays
+    int nums[5];
+    int *dyn_arr=NULL;
+    printf("%p\n", dyn_arr);
+    //printf("%d\n", *dyn_arr);
+    dyn_arr=malloc(20 * sizeof(int));
+    printf("%d\n", dyn_arr[0]);
+    // &dyn_arr[0] == dyn_arr
+    return 0;
+}
+
+void swap (int * a, int * b)
+{
+    /* temp = a
+    a = b;
+    b = temp
+    */
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
