b0b86ce9a29b13ebbfa3e4c22d7c81eb7a96da85
Nathan Eloe <powerofazure@gmail.com>
2023-08-29 10:16:35 -0500

    Demonstrate basic control structures

---
+++ fall23/control.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    srand(time(NULL));
+    int randval;
+    for (int i=9; i>0; i-=1)
+    {
+	if (i%2 == 0)
+	    printf("%d\n", i);
+    }
+    bool ean = true;
+    while (ean)
+    {
+	randval = rand();
+	printf("%d\n", randval);
+	ean = (randval % 11 == 0);
+    }
+    return 0; 
+}
