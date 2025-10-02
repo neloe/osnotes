e156dcf16b3c1e41a6ce3438882be81ec386513c
Nathan Eloe <nathane@nwmissouri.edu>
2025-10-02 09:39:58 -0500

    Add initial fork notes

---
+++ fall25/forkstuff.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main()
+{
+  int cpid = fork();
+  srand(time(NULL)+cpid);
+  if (cpid == 0)
+  {
+    for (int i=0; i<50; i++)
+    {
+      printf("--%d\n", rand()%100);
+    }
+/*    printf("%d nap time!\n", cpid);
+    sleep(10);
+    printf("I'm awake\n");*/
+  }
+  else
+  {
+    for (int i=0; i<50; i++)
+      printf("++%d\n", rand()%100);
+    /*printf("%d yaaawwwwn\n", cpid);
+    sleep(12);
+    printf("Stupid alarm\n");*/
+  }
+  return 0;
+}
