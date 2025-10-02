7c14ee7b262d65d5cf454e13947b236abf74a4a7
Nathan Eloe <nathane@nwmissouri.edu>
2025-10-02 09:57:54 -0500

    Add execvp example

--- fall25/forkstuff.c
+++ fall25/forkstuff.c
@@ -3,16 +3,18 @@
 #include <time.h>
 #include <stdlib.h>
 
-int main()
+int main(int argc, char* argv[])
 {
   int cpid = fork();
   srand(time(NULL)+cpid);
   if (cpid == 0)
   {
-    for (int i=0; i<50; i++)
+/*    for (int i=0; i<50; i++)
     {
       printf("--%d\n", rand()%100);
-    }
+    }*/
+    char * args[] = {"ls", "-alh", NULL};
+    execvp("ls", args);
 /*    printf("%d nap time!\n", cpid);
     sleep(10);
     printf("I'm awake\n");*/
