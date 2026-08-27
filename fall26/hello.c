58f27783433fa32d964e3841b3f399c488a57c84
Nathan Eloe <nathane@nwmissouri.edu>
2026-08-27 10:45:21 -0500

    Create hello.c (f26)

---
+++ fall26/hello.c
@@ -0,0 +1,18 @@
+// single line comment
+/**
+ * @author name
+ * @brief Hello world in C!
+ */ 
+
+#include <stdio.h>
+
+int main() // or int main(int argc, char* argv[])
+{
+  int x = 10;
+  int y; // declaration
+  y = 15; // initialization, but for functions: definition
+  puts("Again hello!");
+  printf("Hello world!\n");
+  printf("x is %d\n", x);
+  return 0;
+}
